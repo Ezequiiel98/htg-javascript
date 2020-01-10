22d54543adf2f1f8e578ff682513f6c6377d7dcf
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-01-10 16:22:05 -0300

    Destaco comentario

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,6 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.');
+
+
+const comentario = document.querySelector('.comment:nth-child(3) p');
+
+comentario.classList.add('comment-fav');
