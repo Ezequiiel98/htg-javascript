0c3638ce96cb8bdbe20014c120d3db947fbc1842
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-01-10 16:29:14 -0300

    Resuelvo ejercicio 2

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,6 +1,15 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.');
 
 
+/* EJERCICIO 1 */
 const comentario = document.querySelector('.comment:nth-child(3) p');
 
-comentario.classList.add('comment-fav');
+comentario.classList.add('comment-fav'); 
+
+/* EJERCICIO 2 */
+
+const hrs = document.querySelectorAll('hr');
+
+for(let i=0; i < hrs.length; i++){
+    hrs[i].classList.add('new-hr');
+}
