82e141698d2bbceb0ce8395f3c8abcb00a8fe085
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-01-13 17:18:38 -0300

    agrego ejercicio 1 y 2

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -2,15 +2,32 @@
 
 const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
+const btnEnviar = document.querySelector('#btn-enviar');
+const mensajeError = document.querySelector('.mensaje-error');
+const labelNombre = document.querySelector('.input-label-nombre');
+const mensajeForm = document.querySelector('.mensaje-form');
 
-function recibirNombre(event) {
+function formularioEnviado(event) {
   event.preventDefault();
-  console.log(event);
-}
+  btnEnviar.classList.add('oculto');
+  mensajeForm.classList.remove('oculto');
+} 
 
 function interceptarNombre(event) {
-  console.log(event.target.value);
-}
+   let nombre = '';
+   nombre += event.target.value;
+   if(nombre.length <= 4 || nombre.length >= 50){
+     inputNombre.classList.add('input-error');
+     mensajeError.classList.remove('oculto');
+     labelNombre.classList.add('error');
+   }else{
+     inputNombre.classList.remove('input-error');
+     mensajeError.classList.add('oculto');
+     labelNombre.classList.remove('error');
 
-formulario.addEventListener('submit', recibirNombre);
+   }
+}                                                                                                                                                        
+
+ formulario.addEventListener('submit', formularioEnviado);
 inputNombre.addEventListener('input', interceptarNombre);
+                                   
